2ba8863d7853da41cc403b977d47bb49cc46ab3b
Hmddev <albert.ghaderi@gmail.com>
2023-09-28 16:50:31 +0330

    u

--- streamlit_code.py
+++ streamlit_code.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import numpy as np
 # import matplotlib.pyplot as plt
-from ZEMO import zemo 
+import ZEMO
 
+st.title("iFiMAS (The best Financial Markets Analysis System)")
+
+symbol = st.sidebar.text_input("Enter a crypto symbol:")
 uploaded_files = st.file_uploader("Choose an image", accept_multiple_files=True)
+
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
 
+Order = st.sidebar.text_input("Enter an Order number:")
+
 st.image(bytes_data, caption='Original Image')
 SZ=np.shape(bytes_data)
-Order=[10,45,46]
 
 for i in range(3):
-   ZBFSTR=zemo.zernike_bf(SZ[0],Order[i],1)
-   Z=zemo.zernike_mom(np.double(bytes_data),ZBFSTR)
-   I=zemo.zernike_rec(Z,SZ[0],ZBFSTR)
+   ZBFSTR=ZEMO.zernike_bf(SZ[0],Order[i],1)
+   Z=ZEMO.zernike_mom(np.double(bytes_data),ZBFSTR)
+   I=ZEMO.zernike_rec(Z,SZ[0],ZBFSTR)
 
 st.image(I)
