87514005b55e0e6dfd8bdeeb3aeacd017454a1e3
Hmddev <albert.ghaderi@gmail.com>
2023-09-28 16:56:23 +0330

    u

--- streamlit_code.py
+++ streamlit_code.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 # import matplotlib.pyplot as plt
 import ZEMO
+from ZEMO import zemo
 
 st.title("iFiMAS (The best Financial Markets Analysis System)")
 
@@ -17,8 +18,8 @@ st.image(bytes_data, caption='Original Image')
 SZ=np.shape(bytes_data)
 
 for i in range(3):
-   ZBFSTR=ZEMO.zernike_bf(SZ[0],Order[i],1)
-   Z=ZEMO.zernike_mom(np.double(bytes_data),ZBFSTR)
-   I=ZEMO.zernike_rec(Z,SZ[0],ZBFSTR)
+   ZBFSTR=zemo.zernike_bf(SZ[0],Order[i],1)
+   Z=zemo.zernike_mom(np.double(bytes_data),ZBFSTR)
+   I=zemo.zernike_rec(Z,SZ[0],ZBFSTR)
 
 st.image(I)
